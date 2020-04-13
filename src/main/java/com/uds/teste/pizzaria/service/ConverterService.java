0290f18920c4ba1cd4a7f7d250ad53a73e9a6fa6
feliciano <felicianojrsousa@gmail.com>
2020-04-13 19:22:49 -0300

    ajuste

--- src/main/java/com/uds/teste/pizzaria/service/ConverterService.java
+++ src/main/java/com/uds/teste/pizzaria/service/ConverterService.java
@@ -5,6 +5,5 @@ import com.uds.teste.pizzaria.model.AbstractEntity;
 
 public interface ConverterService<D extends AbstractDTO, E extends AbstractEntity> {
 	E entidade(D dto);
-
 	D dto(E entity);
 }
